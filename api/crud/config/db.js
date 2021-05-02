ba4c34ab82dc925db428eb7af2e7d1f0efd2e1f8
Gene Romuga <g.romuga@irri.org>
2021-05-02 19:20:46 +0800

    Add db config

---
+++ api/crud/config/db.js
@@ -0,0 +1,12 @@
+require('dotenv').config({path:'config/.env'})
+const Pool = require('pg').Pool;
+
+const pool = new Pool({
+    host : process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user : process.env.DB_USERNAME,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_NAME
+});
+
+module.exports = pool;
